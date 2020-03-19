6de213ce7bd19ea98365f4af80b2879cfa856a3d
IvanPustovit <pustovit.for@gmail.com>
2020-03-19 23:50:38 +0200

    Homework12_prettier

--- src/m12/fetchCountries.js
+++ src/m12/fetchCountries.js
@@ -2,12 +2,12 @@
 
 // let coutries = 'swa'
 
-export default function fetchCountries(name){
-    const country = document.querySelector('.list-country')
-    const countryInfo = document.querySelector('.country-info')
-    country.innerHTML = ''
-    countryInfo.innerHTML = ''
-    return fetch(`http://restcountries.eu/rest/v2/name/${name}`)
+export default function fetchCountries(name) {
+  const country = document.querySelector('.list-country')
+  const countryInfo = document.querySelector('.country-info')
+  country.innerHTML = ''
+  countryInfo.innerHTML = ''
+  return fetch(`https://restcountries.eu/rest/v2/name/${name}`)
     .then(res => res.json())
-    
+
 }
--- src/m12/render.js
+++ src/m12/render.js
@@ -1,51 +1,50 @@
+import country from "./fetchCountries";
+import PNotify from "../../node_modules/pnotify/dist/es/PNotify.js";
+import info from "./countryInfo.hbs";
 
-import country from './fetchCountries';
-import PNotify from '../../node_modules/pnotify/dist/es/PNotify.js';
-import info from './countryInfo.hbs'
+const input = document.querySelector("#search-form");
 
-const input = document.querySelector('#search-form')
+input.addEventListener("input", inputSearch);
 
-input.addEventListener('input', inputSearch)
-
-function inputSearch(e){
-    e.preventDefault()
-    const value = input.value;
-    if(value.length < 3){
-        // PNotify.alert('Введіть більше символів')    
-    }
-        else{
-       country(value)
-    .then(data => {
-        if(data.length == 1){
-            renderInfoCoutry(data)
+function inputSearch(e) {
+  e.preventDefault();
+  const value = input.value;
+  if (value.length < 3) {
+    // PNotify.alert('Введіть більше символів')
+  } else {
+    country(value)
+      .then(data => {
+        if (data.length == 1) {
+          renderInfoCoutry(data);
         }
-        if(data.length > 10){
-            PNotify.alert('Уточніть Ваш пошук більш детальніше')
+        if (data.length > 10) {
+          PNotify.alert("Уточніть Ваш пошук більш детальніше");
+        } else {
+          data.forEach(element => {
+            let name = element;
+            render(element);
+          });
         }
-        else{
-        data.forEach(element => {
-            let name = element
-            render(element)
-        }
-    )}})
-    .catch(err => {console.log(err); PNotify.alert('Уточніть Ваш пошук')}
-    )
-}}
-
-//Розмітка списку країн
-function renderList(countryName){
-    return `<li>${countryName}</li> `
+      })
+      .catch(err => {
+        console.log(err);
+        PNotify.alert("Уточніть Ваш пошук");
+      });
+  }
 }
 
-function render (obj){
-    const listCountry = document.querySelector('.list-country')
-    listCountry.insertAdjacentHTML('beforeend', renderList(obj.name))
+//Розмітка списку країн
+function renderList(countryName) {
+  return `<li>${countryName}</li> `;
+}
+
+function render(obj) {
+  const listCountry = document.querySelector(".list-country");
+  listCountry.insertAdjacentHTML("beforeend", renderList(obj.name));
 }
 
 //Розмітка опису однієї країни
-function renderInfoCoutry(obj){
-    const countryInfo = document.querySelector('.country-info')
-    countryInfo.insertAdjacentHTML('beforeend', info(obj))
+function renderInfoCoutry(obj) {
+  const countryInfo = document.querySelector(".country-info");
+  countryInfo.insertAdjacentHTML("beforeend", info(obj));
 }
-
-
